5f421163e18848502baad9eed084951731757100
Vishal Umtol <vau4869@gmail.com>
2024-12-21 23:40:49 +0530

    Completed menu items

---
+++ src/app/layout/menus/main.menu.ts
@@ -0,0 +1,9 @@
+export const MAINMENU = [
+    {
+        label: 'Home',
+        items: [
+            { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
+            { label: 'Crud', icon: 'pi pi-file-edit', routerLink: ['/crud'] }
+        ]
+    }
+]
